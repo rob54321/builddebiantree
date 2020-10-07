1bec47045001444acdb473d7d59b3f8207a38af5
Robert <robertkey@mighty.co.za>
2020-10-07 07:13:16 +0200

    improved help

--- usr/local/bin/bdt.pl
+++ usr/local/bin/bdt.pl
@@ -427,7 +427,7 @@ sub usage {
 -K import secret key from \"secretkeyfile\" if blank defaults to $secretkeyfile\
 -l list debian packages in subversion\
 -p [\"pkg1 pkg2 ...\"] extract package latest release from subversion -> build -> add to distribution tree\
--t [\"pkg1 pkg2 ...\"] extract package from trunk in subversion, build->add to archive tree\
+-t [\"pkg1 pkg2 ...\"] extract package from trunk/root in subversion, build->add to archive tree\
 -g [\"pkg1 pkg2 ...\"] extract package from git project branch, build->add to tree\
 -d [\"pkg1 pkg2 ...\"] extract package from git dev branch, build->add to tree\
 -r [\"dir1 dir2 ...\"] recurse directory for deb packages list containing full paths, build -> add to archive\
